39e0adc5d65c28a661fef7d7914ec8a5b4ff2713
Brayan Ivan <brayan.perez56@uabc.edu.mx>
2024-02-14 07:36:24 -0800

    first commit

---
+++ Practica 1/Practica 1.1.py
@@ -0,0 +1,9 @@
+
+#13/02/2024
+#Practica 1.1 de Laboratorio 
+#Brayan Ivan Perez Ventura - 00372781
+
+precioProducto = 34.96
+descuento = 34.96 * 0.15
+precioCOnDescuento = precioProducto - descuento
+print(f"El precio del producto con descuento es: {precioCOnDescuento:.2f}")
---
+++ Practica 1/Practica 1.2.py
@@ -0,0 +1,16 @@
+
+# 13/02/2024
+#Practica 1.2 de Laboratorio 
+#Brayan Ivan Perez Ventura - 00372781
+
+x = 2
+h = 5
+
+result = ((1 / (x + h) ** 2) - (1 / x**2)) / h
+
+print(f"El resultado de la primera formula es: {result:.4f}")
+
+result = -((2*x) + h) / (((x+h)**2) * x**2)
+
+print(f"El resultado de la segunda formula es: {result:.4f}")
+
